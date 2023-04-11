ca889f9a116b65271dcb78ddf8340e0ccfa5c638
Oleksii Holodniak <holodniakwork@gmail.com>
2023-04-11 18:26:05 +0300

    fix upload size

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors());
+app.use(express.json({ limit: "50mb" }));
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/api", upload);
